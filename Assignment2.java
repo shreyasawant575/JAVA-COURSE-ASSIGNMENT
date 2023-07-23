33039641225f79322165efd433b6658199581f03
shreyasawant575 <139642112+shreyasawant575@users.noreply.github.com>
2023-07-23 17:59:34 +0530

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,34 @@
+class Helloworld {
+    public static void main(String[] args) {
+    //  class- Buffereader
+    import java.io.*;
+class Helloworld {
+    public static void main(String[] args) {
+    BufferedReader b = new BufferedReader(new InputStreamReader(System.in));  
+        int c=4;
+        int d=3;
+       System.out.println(c*d);
+       // if else statement
+       import java.io.*;
+class Helloworld {
+    public static void main(String[] args) {
+     int marks=70;
+        if(marks>78){
+            System.out.println("passed");
+        }
+        else{
+            System.out.println("failed"); 
+        }
+    
+    }
+}
+
+    
+    
+
+    
+    
+
+    
+    
+  
